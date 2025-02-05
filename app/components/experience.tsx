73aaee976ea3e10c5f1cd15f0132a5475e8bf704
Pauline <pauline.benghouzi@gmail.com>
2025-02-05 09:41:48 +0100

    Tel

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -6,16 +6,15 @@ import { useSectionInView, useWindowSizeHook } from '../lib/hooks'
 
 export default function Experience() {
   const width = useWindowSizeHook()
-
   const { ref } = useSectionInView('Expériences', width > 700 ? 0.5 : 0.3)
+
   return (
-    <section
-      ref={ref}
-      id="experience"
-      className="scroll-mt-24"
-    >
+    <section ref={ref} id="experience" className="scroll-mt-24">
       <Header>Expériences</Header>
       <Timeline data={experiences} />
+
+      {/* Ajout d'un espace uniquement sur téléphone */}
+      <div className="h-4 max-sm:h-16" />
 
       <Header>Formation</Header>
       <Timeline data={formation} />
